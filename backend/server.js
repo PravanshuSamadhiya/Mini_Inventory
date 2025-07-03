5c21e76b58710dd6d21c2820bfa36cca9776943d
Pravanshu Samadhiya <pravanshusamadhiya685@gmail.com>
2025-07-03 18:59:12 +0530

    CORS Fixed

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express();
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://mini-inventory-obqc.vercel.app'],
+  origin: ['http://localhost:5173', 'https://mini-inventory-ashy.vercel.app/'],
   credentials: true
 };
 app.use(cors(corsOptions));
