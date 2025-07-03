6c836d9ddded6f11f2363d5514bc9b8d93dad4c1
Pravanshu Samadhiya <pravanshusamadhiya685@gmail.com>
2025-07-03 15:05:34 +0530

    Database error

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,13 +2,20 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const connectDB = async() => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log("Mongo URI:", process.env.MONGO_URI);
-    } catch (error) {
-       console.error("MongoDB connection error:", error);
-    }
-}
+const connectDB = async () => {
+  try {
+    console.log("Mongo URI on Vercel:", process.env.MONGO_URI); 
+
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000, 
+    });
+
+    console.log("✅ MongoDB connected successfully");
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error);
+  }
+};
 
 export default connectDB;
