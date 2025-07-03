5599b703efcf768acb69a87eb41a192ba5cb92e2
Pravanshu Samadhiya <pravanshusamadhiya685@gmail.com>
2025-07-03 18:55:27 +0530

    backend link pasted

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://mini-inventory-three.vercel.app/api',
+  baseURL: 'https://mini-inventory-5dck.onrender.com/api',
   withCredentials: true,
 });
 
